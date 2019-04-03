b4526d4b40e664a9e23a3182df437f90549bf388
thomasutsey <tomutsey@yahoo.com>
2019-04-03 00:19:46 -0400

    debug

--- routes/cohortsroute.js
+++ routes/cohortsroute.js
@@ -88,5 +88,22 @@ if(result > 0) {
   }
 });
 
+// `[GET] /api/cohorts/:id/students` returns all students for the cohort with the specified `id`.
+router.get('/:id/students', async (req, res)=>{
+  try{
+ const students = await db('students').where({cohortID:req.params.id })
+ const result = await db('cohorts').where({ id: req.params.id });
+ if(result){
+ res.status(200).json(students)
+ }else{
+  res.status(404).json({message: 'Records not found'})
+ }
+  }catch(error){
+    const message = errors[error.errno] || 'Retry different value server error';
+    res.status(500).json({message, error});
+  }
+});
+
+
 module.exports = router;
 
