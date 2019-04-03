8bb48093377b381e753c7805e1d10438693784d2
thomasutsey <tomutsey@yahoo.com>
2019-04-02 22:53:10 -0400

    add knex migration for cohorts and students database

---
+++ migrations/20190402220932_students.js
@@ -0,0 +1,16 @@
+// New changes needed
+exports.up = function(knex) {
+  return knex.schema.createTable('students', (tbl)=>{
+    tbl.increments();//primary key command defaults as ID
+
+    tbl.string('name',128)
+       .notNullable()
+       .unique();
+
+    
+  })
+};
+//undo changes
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExist('students');
+};
--- routes/cohortsroute.js
+++ routes/cohortsroute.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../data/lambda.sqlite3');
+
+const errors = { 
+  '19':'Another entree has the same value no duplicates'
+}
+
+router.post('/',async (req, res) => {
+  try {
+    const [id] = await db('cohorts').insert(req.body);
+    const result = await db('cohorts')
+    .where({id})
+    .first()
+    res.status(201).json(result);
+  } catch (error) {
+    // log error to database
+    const message = errors[error.errno] || 'Retry different value server error';
+    res.status(500).json({message, error});
+  }
+  
+})
+
+
+
 
 router.get('/', async (req, res) => {
   res.send(`
